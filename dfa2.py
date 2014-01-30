47a75e5d28385b066b355f96ac8bc8a44dfe89b6
arnaudMDM <arnaud.mdm@gmail.com>
2014-01-30 18:12:08 +0900

    k8 works

--- dfa2.py
+++ dfa2.py
@@ -16,7 +16,7 @@ def k9_K9J(ckdk, ej):
         sub2 = subytesPoint(j ^ ej)
         diff2 = sub1 ^ sub2
         if diff2 == ckdk:
-            return sub1, sub2
+            return j, j ^ ej
     else:
         raise "error line 21"
 
@@ -59,10 +59,9 @@ def k9(correct):
                     break
             else:
                 raise "error line 54"
-    print finish
     return result
 
-def K8_Ej(j, cjdj):
+def k8_Ej(j, cjdj):
     if j > 0:
         return [cjdj]
     else:
@@ -74,15 +73,16 @@ def K8_Ej(j, cjdj):
                     break
         return result
 
-def K8_Fj(k9K, cjM2djM2):
+def k8_Fj(k9K, cjM2djM2):
     for i in range(256):
         if cjM2djM2 == (subytesPoint(k9K) ^ subytesPoint(k9K ^ i)):
             return i
 
-def K8_K8J(fj, ej):
+def k8_K8J(fj, ej):
     for i in range(256):
         if fj == (subytesPoint(i ^ ej) ^ subytesPoint(i)):
-            return i
+            return i ^ ej, i
+    return -1,-1
 
 def k8(correct, k9):
     result = [-1,-1,-1,-1]
@@ -92,28 +92,38 @@ def k8(correct, k9):
         diff = [[correct[i][j] ^ fault[i][j] for j in range(COL)] for i in range(RAW)]
         for i in range(RAW):
             if i not in finish[2] and diff[i][COL - 1] == 0 and diff[(i - 1) % 4][COL - 1] != 0 and diff[(i - 2) % 4][COL - 1] != 0 and diff[(i - 3) % 4][COL - 1] != 0:
-                lstEj = K8_Ej(i, diff[(i - 1) % 4][COL - 1])
-                fj = K8_Fj(k9[(i - 2) % 4], diff[(i - 3) % 4][COL - 1])
+                print finish 
+                lstEj = k8_Ej(i, diff[(i - 1) % 4][COL - 1])
+                fj = k8_Fj(k9[(i - 2) % 4], diff[(i - 3) % 4][COL - 1])
                 if i not in finish[0]:
                     temp = [[],[],[]]
                     for j in lstEj:
-                        sub1, sub2 = K8_K8J(fj, j)
+                        sub1, sub2 = k8_K8J(fj, j)
+                        if sub1 == -1:
+                            continue
                         temp[0].append(j)
                         temp[1].append(sub1)
                         temp[2].append(sub2)
+                    if len(temp[0]) == 0:
+                        break
                     finish[1].append(temp)
+                    finish[0].append(i)
                 else:
                     indice = finish[0].index(i)
-                    temp = []
+                    temp = [[],[]]
                     for j in lstEj:
                         if j not in finish[1][indice][0]: 
-                            sub1, sub2 = K8_K8J(fj, j)
-                            temp.append([sub1,sub2])
+                            sub1, sub2 = k8_K8J(fj, j)
+                            temp[0].append(sub1)
+                            temp[1].append(sub2)
                     length = len(finish[1][indice][1])
+                    if length == 0:
+                        break
                     if length > 1:
                         k = 0
                         while k < len(finish[1][indice][1]):
-                            if finish[1][indice][1][k] not in temp[1] and finish[1][indice][2][k] not in temp[2]:
+                            temp2 = finish[1][indice]
+                            if temp2[1][k] not in temp[0] and temp2[1][k] not in temp[1] and temp2[2][k] not in temp[1] and temp2[2][k] not in temp[0]:
                                 del(finish[1][indice][0][k])
                                 del(finish[1][indice][1][k])
                                 del(finish[1][indice][2][k])
@@ -121,10 +131,15 @@ def k8(correct, k9):
                                 k += 1
                     else:
                         for k in range(len(finish[1][indice][1])):
-                            if finish[1][indice][1][k] in temp[1] :
-                                result[i] = finish[1][indice][1][k] ^ k9[i]
-                            elif finish[1][indice][2][k] in temp[2]:
-                                result[i] = finish[1][indice][2][k] ^ k9[i]
+                            temp2 = finish[1][indice]
+                            # print finish
+                            # print indice
+                            # print k
+                            # print temp
+                            if temp2[1][k] in temp[0] or temp2[1][k] in temp[1]:
+                                result[i] = temp2[1][k] ^ k9[i]
+                            elif temp2[2][k] in temp[1] or temp2[2][k] in temp[0]:
+                                result[i] = temp2[2][k] ^ k9[i]
                             else:
                                 break
                             finish[2].append(i)
@@ -232,10 +247,12 @@ def dfa():
     correct = aes(text, key)
     k = k9(correct)
     print k
+    k = k8(correct, k)
+    print k
     k = key
     for i in range(10):
         k = keySchedule(k, i)
-    print zip(*k)
+    print zip(*k)[38]
 
 if __name__ == "__main__":
     dfa()
